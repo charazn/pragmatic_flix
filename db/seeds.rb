2ad520b10d4e783c4f84ad1b7eafe3fad5e26cc8
JasonZ <jason.zeng@tinkerbox.com.sg>
2015-06-26 10:31:14 +0800

    Complete Level 6 Migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+movies = [
+ [
+   title: "Iron Man", 
+   rating: "PG-13",
+   total_gross: 318412101,
+   description: "Tony Stark builds an armored suit to fight the throes of evil",
+   released_on: "2008-05-02"
+   ], 
+ [
+   title: "Superman", 
+   rating: "PG",
+   total_gross: 134218018,
+   description: "Clark Kent grows up to be the greatest super-hero",
+   released_on: "1978-12-15"
+   ], 
+ [
+   title: "Spider-Man", 
+   rating: "PG-13",
+   total_gross: 403706375,
+   description: "Peter Parker gets bit by a genetically modified spider",
+   released_on: "2002-05-03"
+   ]
+]
+
+
+movies.each do | movie |
+ Movie.create(movie)
+end
