297c1a5d5300d09cb0eb5b0da5f4dd2020019e75
JasonZ <jason.zeng@tinkerbox.com.sg>
2015-06-29 13:19:52 +0800

    Complete Level 12 Forms:Creating

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,16 +11,25 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
-  def update #Instance variables in Rails do not live on after the action run
+  def update 
     @movie = Movie.find(params[:id])
     @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
     redirect_to @movie
   end
 
   private
 
     def movie_params
-      params.require(:movie).permit(:title, :description, :rating, :release_on, :total_gross)
-      #Or .permit! if you want all of a movie's attributes to be updatable without listing them out
+      params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,6 @@ Rails.application.routes.draw do
 
   root 'movies#index'
 
-  get '/movies' => 'movies#index'
-  get '/movies/:id' => 'movies#show', as: 'movie'
-  get '/movies/:id/edit' => 'movies#edit', as: 'edit_movie'
-  patch 'movies/:id' => 'movies#update'
-  #By convention in Rails, POST requests are used to create things. 
-  #But Rails knows that we're trying to update a movie, not create a movie. 
-  #By convention in Rails, PATCH requests are used to update things. 
-  #Rails effectively overrides the HTTP verb by adding a hidden field named _method 
-  #with a value of patch. 
-  #(It has to fake it with a hidden field because web browsers can't natively 
-  #send PATCH requests.)
+  resources :movies
 
 end
