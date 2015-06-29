f859a6722fa95555a3481fde593b5e571eab7267
JasonZ <jason.zeng@tinkerbox.com.sg>
2015-06-29 14:47:07 +0800

    Complete Level 14 Destroy

---
+++ spec/features/delete_movie_spec.rb
@@ -0,0 +1,15 @@
+describe "Delete a movie" do
+
+  it "deletes selected movie and redirects to listing page" do 
+
+    movie = Movie.create(movie_attributes)
+
+    visit movie_url(movie)
+
+    click_link 'Delete Movie'
+    expect(current_path).to eq(movies_path)
+    expect(page).not_to have_text(movie.title)
+
+  end
+
+end
