dbfb93e24778d4536c08cac526220a1925fff33b
JasonZ <jason.zeng@tinkerbox.com.sg>
2015-06-29 09:34:37 +0800

    Complete Level 10.7

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,38 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-movies = [
+movies = [ #or Movie.create!([{title:"", rating:"", total_gross:0, description:"", released_on:""}])
  [
    title: "Iron Man", 
    rating: "PG-13",
-   total_gross: 318412101,
+   total_gross: 318_412_101,
    description: "Tony Stark builds an armored suit to fight the throes of evil",
    released_on: "2008-05-02"
    ], 
  [
    title: "Superman", 
    rating: "PG",
-   total_gross: 134218018,
+   total_gross: 134_218_018,
    description: "Clark Kent grows up to be the greatest super-hero",
    released_on: "1978-12-15"
    ], 
  [
    title: "Spider-Man", 
    rating: "PG-13",
-   total_gross: 403706375,
+   total_gross: 403_706_375,
    description: "Peter Parker gets bit by a genetically modified spider",
    released_on: "2002-05-03"
+   ],
+ [
+   title: "Catwoman",
+   rating: "PG-13",
+   total_gross: 40_200_000,
+   description: "Patience Philips has a more than respectable career as a graphic designer",
+   released_on: "2004-07-23"
    ]
 ]
 
 
 movies.each do | movie |
- Movie.create(movie)
+ Movie.create!(movie) #bang method will throw exception if movie is not created
 end
