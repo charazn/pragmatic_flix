d585faf8ffdc2188f6ac979faf10b1da581e8f28
Char <charazn37@gmail.com>
2015-08-03 14:18:45 +0800

    Fix seed cast to array

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ movies = [
   total_gross: 318_412_101,
   description: "Tony Stark builds an armored suit to fight the throes of evil",
   released_on: "2008-05-02",
-  cast: {"Robert Downey Jr.", "Gwyneth Paltrow", "Terrence Howard"},
+  cast: ["Robert Downey Jr.", "Gwyneth Paltrow", "Terrence Howard"],
   director: "Jon Favreau",
   duration: 126,
   image_file_name: "ironman.jpg"
@@ -25,7 +25,7 @@ movies = [
   total_gross: 134_218_018,
   description: "Clark Kent grows up to be the greatest super-hero",
   released_on: "1978-12-15",
-  cast: {"Christopher Reeve", "Margot Kidder", "Gene Hackman"},
+  cast: ["Christopher Reeve", "Margot Kidder", "Gene Hackman"],
   director: "Richard Donner",
   duration: 143,
   image_file_name: "superman.jpg"
@@ -36,7 +36,7 @@ movies = [
   total_gross: 403_706_375,
   description: "Peter Parker gets bit by a genetically modified spider",
   released_on: "2002-05-03",
-  cast: {"Tobey Maguire", "Kirsten Dunst", "Willem Dafoe"},
+  cast: ["Tobey Maguire", "Kirsten Dunst", "Willem Dafoe"],
   director: "Sam Raimi",
   duration: 121,
   image_file_name: "spiderman.jpg"
@@ -47,7 +47,7 @@ movies = [
   total_gross: 40_200_000,
   description: "Patience Philips has a more than respectable career as a graphic designer",
   released_on: "2004-07-23",
-  cast: {"Halle Berry", "Sharon Stone", "Benjamin Bratt"},
+  cast: ["Halle Berry", "Sharon Stone", "Benjamin Bratt"],
   director: "Jean-Christophe 'Pitof' Comar",
   duration: 101,
   image_file_name: "catwoman.jpg"
@@ -58,7 +58,7 @@ movies = [
   total_gross: 387623910,
   description: "The Caped Crusader battles the fire-breathing Gojira",
   released_on: 10.days.from_now,
-  cast: {"Bruce Wayne", "Gojira"},
+  cast: ["Bruce Wayne", "Gojira"],
   director: "Ishiro Honda",
   duration: 211,
   image_file_name: "batman_godzilla.jpg"
@@ -69,7 +69,7 @@ movies = [
   total_gross: 547_126_372,
   description: "Navy SEAL sniper Chris Kyle's pinpoint accuracy saves countless lives on the battlefield and turns him into a legend. Back home to his wife and kids after four tours of duty, however, Chris finds that it is the war he can't leave behind.",
   released_on: "2014-12-25",
-  cast: {"Bradley Cooper", "Sienna Miller", "Kyle Gallner"},
+  cast: ["Bradley Cooper", "Sienna Miller", "Kyle Gallner"],
   director: "Clint Eastwood",
   duration: 133,
   image_file_name: "american_sniper.jpg"
