afd8c8943bf203827ca8d11bb64f5a9095d1e7d3
Char <charazn37@gmail.com>
2015-08-04 15:59:13 +0800

    Complete Level 24 One To Many Logic

--- spec/features/delete_review_spec.rb
+++ spec/features/delete_review_spec.rb
@@ -2,7 +2,6 @@
 # Gave weird database errors, even after installing shoulda-matchers and database_cleaner gems
 
 describe "Delete a review" do
-
   it "deletes selected review and redirects to reviews listing page" do
     movie = Movie.create(movie_attributes)
     review = movie.reviews.create(review_attributes(name: "Roger Ebert"))
